6c252a08609a559db1ab08b51fe80eb816d4794e
Vincent Lauro <vinniehat@gmail.com>
2021-10-04 09:56:35 -0400

    Not final

--- PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
+++ PrimeNumbersAgain/PrimeNumbersAgain/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace PrimeNumbersAgain
@@ -10,50 +11,48 @@ namespace PrimeNumbersAgain
             int n, prime;
             Stopwatch timer = new Stopwatch();
 
-            
+
             // Start Initialization
             PrintBanner();
             n = GetNumber();
             // Initialization Complete
-            
+
             // Start The Algorithm
             timer.Start();
             prime = FindNthPrime(n);
             timer.Stop();
             // Algorithm Complete
-            
-            
-            Console.WriteLine($"\nToo easy.. {prime} is the nth prime when n is {n}. I found that answer in {timer.Elapsed.Seconds}.{timer.Elapsed.Milliseconds} seconds.");
+
+
+            Console.WriteLine(
+                $"\nToo easy.. {prime} is the nth prime when n is {n}. I found that answer in {timer.Elapsed.Seconds}.{timer.Elapsed.Milliseconds} seconds.");
 
             EvaluatePassingTime(timer.Elapsed.Seconds);
         }
 
         static int FindNthPrime(int n)
         {
-            int primeNumber = 0;
-            int primeNumbers = 0;
-            for (int i = 1; primeNumbers <= n; i++) // Set pN to less than or equal to n, which is what we want to find
+            int numberOfPrimes = 0;
+            int primeNthNumber = 0;
+            for (int i = 2; i < 2000000; i++)
             {
-                System.Threading.Thread.Sleep(1); // This gives the PC a buffer between each check
-                bool boolean = true; // This boolean defines whether or not the current number, i, is prime or not
-                
-                // Checking if it can be divisible by any number from 2-one below the number itself
+                if (numberOfPrimes == n) break;
+
                 for (int x = 2; x < i; x++)
                 {
-                    
-                    if (i % x == 0) boolean = false;
-                    Console.WriteLine(i + " ----- " + x);
+                    if (numberOfPrimes == n) break;
+                    if (i % x != 0)
+                    {
+                        numberOfPrimes++;
+                        primeNthNumber = i;
+                    }
                 }
-                
-                // If it is not divisible, then set the primeNumber as the last known pN, and add it to the amt of pN's
-                if (boolean == true)
-                {
-                    primeNumber = i;
-                    primeNumbers++;
-                }
+
+
             }
 
-            return primeNumber;
+
+            return primeNthNumber;
         }
 
         static int GetNumber()
@@ -62,7 +61,7 @@ namespace PrimeNumbersAgain
             while (true)
             {
                 Console.Write("Which nth prime should I find?: ");
-                
+
                 string num = Console.ReadLine();
                 if (Int32.TryParse(num, out n))
                 {
@@ -82,8 +81,8 @@ namespace PrimeNumbersAgain
             Console.WriteLine(".##......##..##....##....##...##..##..........##.");
             Console.WriteLine(".##......##..##..######..##...##..######...####..");
             Console.WriteLine(".................................................\n\n");
-            Console.WriteLine("Nth Prime Solver O-Matic Online..\nGuaranteed to find primes up to 2 million in under 30 seconds!\n\n");
-            
+            Console.WriteLine(
+                "Nth Prime Solver O-Matic Online..\nGuaranteed to find primes up to 2 million in under 30 seconds!\n\n");
         }
 
         static void EvaluatePassingTime(int time)
@@ -103,7 +102,6 @@ namespace PrimeNumbersAgain
             }
 
             Console.ForegroundColor = ConsoleColor.Gray;
-            
         }
     }
 }
